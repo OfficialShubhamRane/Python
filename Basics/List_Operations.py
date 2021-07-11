0f8e05de97a60f854cc061c73fed24108e117b26
Shubham Rane <official.shubhamrane@gmail.com>
2021-07-10 22:03:06 -0400

    Basic list operations

---
+++ Basics/List_Operations.py
@@ -0,0 +1,17 @@
+groceryList = ["Apples","Milk","Chicken Breasts","Bread","Avacado"];
+
+groceryList.append("Bananas")
+
+groceryList.insert(4, "Wine")
+
+print(groceryList.index("Wine"))
+
+print(groceryList);
+
+print(groceryList[0]);
+
+print(groceryList[-1]);
+
+print(groceryList[2-4]);
+
+
