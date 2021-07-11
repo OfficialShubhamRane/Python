39d1962781c485569b37e513aa824202b7b2a754
Shubham Rane <official.shubhamrane@gmail.com>
2021-07-10 22:30:43 -0400

    Tuples

---
+++ Basics/Tuples.py
@@ -0,0 +1,8 @@
+# Tuples are immutable, initialized with ()
+waffle = ("Shubham", "Rajan", "Rane");
+
+print(waffle.index("Shubham"));
+
+# print(waffle.index("Shubham",1));
+
+print( len(waffle) );
