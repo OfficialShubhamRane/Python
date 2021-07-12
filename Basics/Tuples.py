bdc719be93598f7679b6ebd6888ff2d9ed995aa0
Shubham Rane <official.shubhamrane@gmail.com>
2021-07-11 22:08:07 -0400

    Unpacking with tuples

--- Basics/Tuples.py
+++ Basics/Tuples.py
@@ -1,8 +1,10 @@
 # Tuples are immutable, initialized with ()
 waffle = ("Shubham", "Rajan", "Rane");
-
-print(waffle.index("Shubham"));
-
-# print(waffle.index("Shubham",1));
-
+print(waffle.index("Shubha/m"));
+# print(waffle.index("Shubham",1)); # This one throws an error
 print( len(waffle) );
+
+# Unpacking wit Tuples
+coordinates = (40.6474, -73.02765, 238)
+x, y, z = coordinates
+print(x, y, z);
