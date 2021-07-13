6af60647bbe67c077bdce0ac64272297f11b56e1
Shubham Rane <official.shubhamrane@gmail.com>
2021-07-13 00:00:38 -0400

    Packing with tuples

--- Basics/Tuples.py
+++ Basics/Tuples.py
@@ -1,10 +1,18 @@
-# Tuples are immutable, initialized with ()
-waffle = ("Shubham", "Rajan", "Rane");
-print(waffle.index("Shubha/m"));
-# print(waffle.index("Shubham",1)); # This one throws an error
-print( len(waffle) );
+# # Tuples are immutable, initialized with ()
+# waffle = ("Shubham", "Rajan", "Rane");
+# print(waffle.index("Shubha/m"));
+# # print(waffle.index("Shubham",1)); # This one throws an error
+# print( len(waffle) );
 
-# Unpacking wit Tuples
-coordinates = (40.6474, -73.02765, 238)
-x, y, z = coordinates
+# Unpacking with Tuples
+coordinates = (40.6474, -73.02765, 238);
+x, y, z = coordinates;
 print(x, y, z);
+print(coordinates);
+
+# Packing with tuples
+x = -40.8583724;
+y = 73.4758347;
+z = 238;
+coordinates = x,y,z;
+print(coordinates);
