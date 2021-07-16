de8a3a4f1bde05d5a3c017cdc8efcbbb0387ef5b
Shubham Rane <official.shubhamrane@gmail.com>
2021-07-15 20:05:36 -0400

    Code Refactoring

--- ExcelAutomation/ExcelAutomation.py
+++ ExcelAutomation/ExcelAutomation.py
@@ -1,9 +1,17 @@
 import openpyxl as xl
-wb = xl.load_workbook("transactions.xlsx");
-sheet = wb["Sheet1"];
 
-for i in range(2,sheet.max_row + 1):
-    print(sheet.cell(i, 3).value * 0.9);
-    sheet.cell(i, 4).value = sheet.cell(i, 3).value * 0.9;
 
-wb.save("transaction2.xlsx")
+def prcess_workbook(fileName, sheetName):
+    wb = xl.load_workbook(fileName);
+    sheet = wb[sheetName];
+
+    for i in range(2, sheet.max_row + 1):
+        print(sheet.cell(i, 3).value * 0.9);
+        sheet.cell(i, 4).value = sheet.cell(i, 3).value * 0.9;
+
+    sheet.cell(1, 4).value = "Discount(10%)";
+
+    # Overriding current spreadsheet with changes
+    wb.save(fileName)
+
+prcess_workbook("transactions.xlsx","Sheet1");
